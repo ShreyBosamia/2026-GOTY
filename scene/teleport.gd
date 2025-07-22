7e5731abe07a8ffc8d6299113e108fd5a340f29e
Shrey <shreybosamia@outlook.com>
2025-07-21 17:14:16 -0700

    Added particle effects to teleporter. Still need to config it. Also changed camera angle to be lower so there's more depth therefore more like a 2.5D/HD-2D game

--- scene/teleport.gd
+++ scene/teleport.gd
@@ -2,27 +2,32 @@ extends Area3D
 @onready var timer = $Timer
 @export var dest_scene: PackedScene
 var entered_area
+var time_out 
 
 func _on_body_entered(body: PhysicsBody3D):
 	entered_area = true
 	print("entered")
-	#timer.start()
+	timer.start()
 
 
 func _on_body_exited(body: PhysicsBody3D):
 	entered_area = false
 	print("exited")
-	#timer.end()
+	timer.stop()
 	
 func _ready():
 	entered_area = false
-'''
-func _on_timer_timeout():
-	if entered_area == true and dest_scene:
-			get_tree().change_scene_to_packed(dest_scene)
-'''
+	time_out = false
+	print("time_out is false")
+
 
 func _process(_delta):
-	if entered_area == true and dest_scene:
-		if Input.is_action_just_pressed("accept"):
-			get_tree().change_scene_to_packed(dest_scene)
+	if time_out and dest_scene:
+		#if Input.is_action_just_pressed("accept"):
+		get_tree().change_scene_to_packed(dest_scene)
+
+
+func _on_timer_timeout():
+	time_out = true
+	print("Time_out is true")
+	
